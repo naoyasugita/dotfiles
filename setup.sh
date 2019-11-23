92f37197118c7ab398372ae2966d2e7f14ab53be
NaoyaSugita <sugita6878@gmail.com>
2019-11-24 01:27:18 +0900

    installの設定を追加

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -euo pipefail
+
+source lib/echos.sh
+
+function command_exists() {
+  type "$1" &> /dev/null ;
+}
+
+
+: "install brew" && {
+  if ! command_exists brew; then
+    info "installing brew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    warn "brew is already installed"
+  fi
+}
+
+: "install other packages by brew" && {
+  packages=( jq tree wget direnv vim git pyenv pyenv-virtualenv npm mysql caskroom/cask/brew-cask)
+  for package in ${packages[@]}; do
+    if ! brew list | grep $package &> /dev/null; then
+      info "installing ${package}..."
+      brew install ${package}
+    else
+      warn "${package} is already installed"
+    fi
+  done
+  brew cleanup
+}
+
+: "install brew cask" && {
+  packages=( google-chrome alfred iterm2 google-japanese-ime slack \
+   visual-studio-code flux karabiner  )
+  for package in ${packages[@]}; do
+    if ! brew cask list | grep $package &> /dev/null; then
+      info "installing ${package}..."
+      brew cask install ${package}
+    else
+      warn "${package} is already installed"
+    fi
+  done
+  brew cask cleanup
+}
+
+: "install oh-my-zsh" && {
+  if [ ! -e $HOME/.oh-my-zsh ]; then
+    info "installing oh-my-zsh..."
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install
+    info "installing zsh theme..."
+    git clone https://github.com/wesbos/Cobalt2-iterm.git
+    cd Cobalt2-iterm
+    cp cobalt2.zsh-theme ~/.oh-my-zsh/themes/
+  else
+    warn "oh-my-zsh is already installed"
+  fi
+}
+
+ok "Complete!"
