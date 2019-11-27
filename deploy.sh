284d4e9ff26ce491b2f5bc95c6e44c9d5a1dba2c
NaoyaSugita <sugita6878@gmail.com>
2019-11-27 22:58:37 +0900

    deployの設定を追加

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+source lib/echos.sh
+
+readonly DOT_FILES=( .zshrc )
+
+for file in ${DOT_FILES[@]}; do
+  dest=${HOME}/${file}
+  if [ -e ${dest} ]; then
+    warn "[warn] ${dest}: skipped (already exists)"
+  else
+    ln -s $HOME/dotfiles/$file $dest
+    ok "[ok] ${dest}: created"
+  fi
+done
+
+ok "Complete!"
