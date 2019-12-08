916aa356c2bbef61106c2b6d01e739ae754ef226
naoyasugita <sugita6878@gmail.com>
2019-12-08 16:14:39 +0900

    修正

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ if command_exists curl || command_exists wget; then
         wget -O - "$tarball"
 
     fi | tar zxvf dotfiles-master
-
-    mv -f dotfiles-master/* "$DOTPATH"
+    SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+    mv -i "$SCRIPT_DIR/dotfiles-master/*" "$DOTPATH"
     rm -rf dotfiles-master
 else
     echo "curl or wget required"
