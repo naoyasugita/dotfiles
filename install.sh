fd2233dbd140f1f05297d5b7188dc0ce1182683e
naoyasugita <sugita6878@gmail.com>
2019-12-08 15:18:41 +0900

    [wip]http経由でインストールできるようにする

---
+++ install.sh
@@ -0,0 +1,33 @@
+DOTPATH=~/dotfiles
+GITHUB_URL="https://github.com/naoyasugita/dotfiles"
+
+function command_exists() {
+  type "$1" &> /dev/null ;
+}
+
+if command_exists git; then
+    git clone --recursive "${$GITHUB_URL}.git" "$DOTPATH"
+
+elif has curl || command_exists wget; then
+    tarball="https://github.com/naoyasugita/dotfiles/archive/master.tar.gz"
+
+    if command_exists curl; then
+        curl -L "$tarball"
+
+    elif command_exists wget; then
+        wget -O - "$tarball"
+
+    fi | tar zxv
+
+    mv -f dotfiles-master "$DOTPATH"
+else
+    echo "curl or wget required"
+fi
+
+cd ~/dotfiles
+if [ $? -ne 0 ]; then
+    echo "not found: $DOTPATH"
+else
+    sh deploy.sh
+    sh setup.sh
+fi
