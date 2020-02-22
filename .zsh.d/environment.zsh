acbdd94c8fb0fcf099d74d34b3d075fc3c50cd78
naoyasugita <sugita6878@gmail.com>
2020-02-23 02:18:22 +0900

    環境変数を追加

--- .zsh.d/environment.zsh
+++ .zsh.d/environment.zsh
@@ -4,7 +4,6 @@ export LANG=ja_JP.UTF-8
 export GIT_EDITOR=vim
 export EDITOR=vim
 
-
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -12,3 +11,9 @@ eval "$(pyenv init -)"
 
 # direnv
 eval "$(direnv hook zsh)"
+
+# flutter
+export PATH="$PATH:$HOME/dotfiles/flutter/bin"
+
+# Androiid SDK
+export ANDROID_HOME="$HOME/Library/Android/sdk"
