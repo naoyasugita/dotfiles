99fb29f8b0770dc5294adca5bf24c1e9da3dec1b
naoyasugita <sugita6878@gmail.com>
2020-04-28 00:37:29 +0900

    .vimrcを読み込むように修正

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@ set -euo pipefail
 
 source lib/echos.sh
 
-readonly DOT_FILES=( .zshrc .zsh.d )
+readonly DOT_FILES=( .zshrc .zsh.d .vimrc)
 
 for file in ${DOT_FILES[@]}; do
   dest=${HOME}/${file}
   if [ -e ${dest} ]; then
-    warn "[warn] ${dest}: skipped (already exists)"
+    ok "[update] ${dest}: (already exists)"
   else
     ln -s $HOME/dotfiles/$file $dest
-    ok "[ok] ${dest}: created"
+    ok "[create] ${dest}"
   fi
 done
 
