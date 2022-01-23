01bca1e84631b3a0bf6a4293fa14f2269a013782
naoyasugita <sugita6878@gmail.com>
2022-01-24 00:26:18 +0900

    starshipの設定を追加

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,18 @@ set -euo pipefail
 
 source lib/echos.sh
 
-readonly DOT_FILES=( .zshrc .zsh.d .vimrc)
+readonly DOT_FILES=( .zshrc .zsh.d .vimrc .config)
 
 for file in ${DOT_FILES[@]}; do
   dest=${HOME}/${file}
-  if [ -e ${dest} ]; then
+  if [ $file = ".config" ]; then
+    if [ -e ${dest}/starship.toml ]; then
+      ok "[update] ${dest}/starship.toml: (already exists)"
+    else
+      ln -s $HOME/dotfiles/$file/starship.toml ${HOME}/${file}/starship.toml
+      ok "[create] ${dest}"
+    fi
+  elif [ -e ${dest} ]; then
     ok "[update] ${dest}: (already exists)"
   else
     ln -s $HOME/dotfiles/$file $dest
