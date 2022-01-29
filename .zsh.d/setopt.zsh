dfa49b1623b775eef2fe6111271324dd2ba816b3
naoyasugita <sugita6878@gmail.com>
2022-01-29 23:05:33 +0900

    historyを復活

---
+++ .zsh.d/setopt.zsh
@@ -0,0 +1,18 @@
+# history
+HISTFILE=~/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+setopt share_history           # 履歴を他のシェルとリアルタイム共有する
+setopt hist_ignore_all_dups    # 同じコマンドをhistoryに残さない
+setopt hist_ignore_space       # historyに保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks      # historyに保存するときに余分なスペースを削除する
+setopt hist_save_no_dups       # 重複するコマンドが保存されるとき、古い方を削除する
+setopt inc_append_history      # 実行時に履歴をファイルにに追加していく
+
+# 履歴検索機能のショートカット設定
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end # UP
+bindkey "^[[B" history-beginning-search-forward-end # DOWN
